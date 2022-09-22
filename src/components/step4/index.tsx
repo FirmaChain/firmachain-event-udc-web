9cf04828bb1379d2ce19823ddce4fa6386a364fb
chocoSprite <chocosprite21@gmail.com>
2022-09-22 11:29:31 +0900

    update: video reset

--- src/components/step4/index.tsx
+++ src/components/step4/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { Step4Wrapper } from '../../styles';
 
@@ -8,17 +8,22 @@ interface IProps {
 }
 
 const Step4 = ({ isActive, setStep }: IProps) => {
+  const videoRef = useRef<any>();
+
   useEffect(() => {
     if (isActive) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+
       setTimeout(() => {
-        setStep(4);
+        setStep(3);
       }, 4500);
     }
   }, [isActive]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Step4Wrapper>
-      <video muted autoPlay playsInline width={'100%'}>
+      <video muted autoPlay playsInline width={'100%'} ref={videoRef}>
         <source src='/box.mp4' />
       </video>
     </Step4Wrapper>
